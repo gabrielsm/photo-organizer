c7be52faeff15b36080508112e4dbe66310bd229
Thiago Berne <Thiago Berne>
2017-01-31 22:53:19 -0200

    Committer: Thiago Berne <Thiago Berne>
    Utilizando Bower para JS client side.
    Alguns pequenos ajustes no app.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const fs = require("fs-extra");
-const $ = require("jquery");
 const utils = {
 	isNull: function(obj) {
+		return !utils.isNull(obj);
+	},
+
+	notNull: function(obj) {
 		if(typeof(obj) === "string") {
 			return obj != null && obj != undefined && obj.length > 0;
 		}
@@ -10,12 +13,8 @@ const utils = {
 		return obj != null && obj != undefined;
 	},
 
-	notNull: function(obj) {
-		return !isNull(obj);
-	},
-
 	whenNotNull: function(obj, callback) {
-		if(notNull(obj)) {
+		if(utils.notNull(obj)) {
 			callback();
 		}
 	}
@@ -33,30 +32,45 @@ const defaultCfg = {
 	let dataPath = nw.App.dataPath;
 	let cfgFile = path.join(dataPath, ".config");
 
+	log(`App data path: ${dataPath}`);
+	log(`Cfg file: ${cfgFile}`);
+
 	function readConfiguration() {
+		debugger;
+		log("Reading config file...");
+
 		var cfg;
 		if(!fs.existsSync(cfgFile)) {
-			fs.writeFileSync(cfgFile, JSON.stringify(defaultCfg), {encoding: "UTF-8"});
+			log("Config file doesn't exist. Using default config...");
 			cfg = defaultCfg;
 		}
 		else {
+			log("Config file exists. reading it...");
 			cfg = fs.readJsonSync(cfgFile, {encoding: "UTF-8"});
+			log("Found config", cfg);
 		}
 
-		utils.whenNotNull(cfg.srcFolder, () => {
+		utils.whenNotNull(cfg.srcFolder, function() {
+			log(`Found srcFolder: ${cfg.srcFolder}`);
 			$("input[name='srcFolder']").val(cfg.srcFolder);
 		});
 		
-		utils.whenNotNull(cfg.trgFolder, () => {
+		utils.whenNotNull(cfg.trgFolder, function() {
+			log(`Found trgFolder: ${cfg.trgFolder}`);
 			$("input[name='trgFolder']").val(cfg.trgFolder);
 		});
 		
-		utils.whenNotNull(cfg.expression, () => {
+		utils.whenNotNull(cfg.expression, function() {
+			log(`Found expression: ${cfg.expression}`);
 			$("input[name='expression']").val(cfg.expression);
 		});
+
+		log("Finished reading config.");
 	}
 
 	function saveConfiguration() {
+		debugger;
+		log("Saving config file...");
 		var cfg = {
 			srcFolder: $("input[name='srcFolder']").val(),
 			trgFolder: $("input[name='trgFolder']").val(),
@@ -64,12 +78,21 @@ const defaultCfg = {
 		};
 
 		fs.writeJsonSync(cfgFile, cfg, {encoding: "UTF-8"});
+		log("Cfg file written.");
 	}
 
 	$(document).ready(() => {
 		readConfiguration();
 
 		$("input").change(function() {
+			saveConfiguration();
+		});
+
+		$("#read").click(function() {
+			readConfiguration();
+		});
+
+		$("#write").click(function(){
 			saveConfiguration();
 		});
 	});
